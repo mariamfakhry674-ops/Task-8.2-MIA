cbe09ca0e025cd328a47682e84a680390ba8df28
Mariam Alalem <mariamfakhry674@gmail.com>
2026-08-24 23:26:01 +0300

    add motor driver pins file

---
+++ pins.h
@@ -0,0 +1,19 @@
+#include <Arduino.h>
+#ifndef PINS_H
+#define PINS_H
+
+// define pins in L298N
+#define IN1 PA8
+#define IN2 PA9
+#define ENA PA6
+
+// define pins in cryton MD10A
+#define DIR PB12
+#define PWM PA7
+
+// define pins in BTS7960
+//two pins for PWM control speed 
+#define RPWM PB6
+#define LPWM PB7
+
+#endif
